d009a9288dcf9cb2a973f30b115d1dbcb80a28cd
Fazel <mrmfamin835@yahoo.com>
2024-12-16 23:45:39 +0330

    fix navbar brand link

--- src/Components/NavbarDesigner.js
+++ src/Components/NavbarDesigner.js
@@ -9,7 +9,7 @@ function NavbarDesigner() {
     return (
         <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="dashboard-designer.html">سوال پیچ</a>
+                <Link className={`navbar-brand`} to="/dashboard-designer">سوال پیچ</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
--- src/Components/NavbarPlayer.js
+++ src/Components/NavbarPlayer.js
@@ -9,8 +9,8 @@ function NavbarPlayer() {
     return (
         <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="dashboard-player.html">سوال پیچ</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+            <Link className={`navbar-brand`} to="/dashboard-player">سوال پیچ</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -21,7 +21,7 @@ function NavbarPlayer() {
                             <Link className={`nav-link ${isActive('dashboard-player')}`} to="/dashboard-player">داشبورد</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className={`nav-link ${isActive('question-player-list')} ${isActive('designer-view')}`} to="/question-player-list">مدیریت سوالات</Link>
+                            <Link className={`nav-link ${isActive('question-player-list')} ${isActive('designer-view')} ${isActive('answering-player')}`} to="/question-player-list">مدیریت سوالات</Link>
                         </li>
                         <li className="nav-item">
                             <Link className={`nav-link ${isActive('score-player')} ${isActive('player-view')}`} to="/score-player">جدول امتیازات</Link>
